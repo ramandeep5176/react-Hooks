4a1ac6d2731a70df41f00e16898b844e77bd6c67
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-23 18:17:19 -0400

    multipleFetch

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,16 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [loading, setLoading] = useState(false);
+
+  if (loading) {
+    return <h2>loading...</h2>;
+  }
+  return (
+    <>
+      <h4>hello</h4>
+    </>
+  );
 };
 
 export default MultipleReturns;
