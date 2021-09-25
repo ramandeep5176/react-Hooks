1913721ea987299cca73812c814c1013a9233cf4
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-25 11:07:43 -0400

    ternaryOperators

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,21 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+const [text,setText]=useState('')
 
-  return <h2>short circuit</h2>;
+  const firstValue = text || 'hello world';
+  const secondValue = text && 'hello world';
+
+  return <>
+  {/* <h1>{firstValue}</h1>
+  <h1>value={secondValue}</h1> */}
+
+{text || <h1>raman</h1> }
+{!text && <h1>hello</h1>
+
+}
+
+  </>;
 };
 
 export default ShortCircuit;
