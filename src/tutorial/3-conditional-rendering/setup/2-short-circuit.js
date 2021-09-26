d359ff50672471312700611a881601ab91f68a96
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-09-26 08:03:00 -0400

    showHide

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-const [text,setText]=useState('')
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
-
-  return <>
-  {/* <h1>{firstValue}</h1>
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
   <h1>value={secondValue}</h1> */}
 
-{text || <h1>raman</h1> }
-{!text && <h1>hello</h1>
-
-}
-
-  </>;
+      <h1>{text || "raman"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {/* {text && <h1>hello</h1>} */}
+      {isError && <h1>error..</h1>}
+      {isError ? <h1>ternery1</h1> : <h1>ternary2</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
