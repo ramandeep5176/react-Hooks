ea98d9e029e4765b459021fbd20ea8039e10e3b8
ramandeep5176 <ramandeepgill5176@gmail.com>
2021-10-09 12:11:21 -0400

    tutForRef

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
@@ -6,7 +6,29 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hell");
+  };
+  return (
+    <>
+      <article>
+        <form className="form" onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label htmlFor="firstName">name :</label>
+            <input type="text" id="firstName" name="firstName" />
+          </div>
+          <div className="form-control">
+            <label htmlFor="email">email :</label>
+            <input type="text" id="email" name="email" />
+          </div>
+          <button className="btn" type="submit">
+            add person
+          </button>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
